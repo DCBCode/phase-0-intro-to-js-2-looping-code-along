3228d8785afb87256e970b67b774af38167bd90b
Drew Benson <Drewbenson16@gmail.com>
2023-11-22 13:00:48 -0700

    assignment 9

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let messages = [];
+
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return messages;
+}
+function countDown(num) {
+  if (typeof num !== 'number' || num <= 0 || !Number.isInteger(num)) {
+    console.log('Please enter a positive integer.');
+    return;
+  }
+
+  let i = num;
+
+  while (i >= 0) {
+    console.log(i);
+    i--;
+  }
+}
+
+countDown(10);
+let countdownResult = countDown(10);
+console.log(countdownResult);
